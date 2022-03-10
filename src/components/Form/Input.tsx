e0117b9a1ec451c5c8f66c168f9220b29afed9fd
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-10 18:37:34 -0300

    create input component

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,27 @@
+import { FormControl, FormLabel, Input as ChackraInput, InputProps as ChackraInputProps } from '@chakra-ui/react'
+
+interface InputProps extends ChackraInputProps {
+  name: string
+  label?: string
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChackraInput
+        name={name}
+        id={name}
+        {...rest}
+        focusBorderColor="pink.500"
+        variant="filled"
+        bgColor="gray.900"
+        _hover={{
+          bgColor: 'gray.900'
+        }}
+        size="lg"
+      />
+    </FormControl>
+  )
+}
