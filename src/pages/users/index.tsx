84450301ac09a00e79a21bf3ff74c17461a4e815
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-11 16:53:25 -0300

    pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Checkbox, Flex, Heading, HStack, Icon, Table, Tbody, Td, T
 import { RiAddLine, RiDeleteBinLine, RiPencilFill } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 
@@ -153,6 +154,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
