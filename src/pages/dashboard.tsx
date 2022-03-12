42dc0c51da280f0a9a55673c8dcaec18310163c4
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-12 16:19:37 -0300

    sidebar drawer

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -76,14 +76,14 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignContent="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['4', '6']} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Inscritos da Semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['4', '6']} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
