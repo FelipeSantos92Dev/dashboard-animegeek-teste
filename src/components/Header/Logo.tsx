7c1ceba2e9b39b6a4457151f0cc4e6edff171594
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-11 21:17:45 -0300

    responsive Header

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,12 @@ import { Text } from '@chakra-ui/react'
 
 export default function Logo() {
   return (
-    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+    <Text
+      fontSize={['2xl', '3xl', '4xl']}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
       ThunderGo
       <Text as="span" ml="1" color="orange.500">
         .
