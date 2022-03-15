a5cd9eed9772193eac9032b1b8eeafc6cc6d8fe1
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-15 14:52:23 -0300

    factory and seed miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useEffect } from 'react'
 import { RiAddLine, RiDeleteBinLine, RiPencilFill } from 'react-icons/ri'
 
 import { Header } from '../../components/Header'
@@ -27,6 +28,13 @@ export default function UserList() {
     base: false,
     lg: true
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <Box>
       <Header />
