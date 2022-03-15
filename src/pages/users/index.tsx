fe5c0531a78a7c91a6a37ab11f339cc708e0ab7b
Felipe Santos <dev.felipesantos@gmail.com>
2022-03-15 20:42:38 -0300

    users hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,38 +18,14 @@ import {
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { RiAddLine, RiDeleteBinLine, RiPencilFill } from 'react-icons/ri'
-import { useQuery } from 'react-query'
 
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import useUsers from '../../services/hooks/useUsers'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
-    'users',
-    async () => {
-      const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
-
-      const users = data.users.map((user: any) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          })
-        }
-      })
-
-      return users
-    },
-    {
-      staleTime: 1000 * 5
-    }
-  )
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -67,6 +43,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size={'sm'} color={'gray.500'} ml={'4'} />
+              )}
             </Heading>
             <Link href={'users/create'} passHref>
               <Button
@@ -104,7 +83,7 @@ export default function UserList() {
                 </Thead>
 
                 <Tbody>
-                  {data.map((user: any) => {
+                  {data?.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={['4', '4', '6']}>
